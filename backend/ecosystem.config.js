dfcadcbed1740580249d95d0f1bdea636d4a38a1
Rita Rixter <ritarixter@mail.ru>
2023-02-13 20:11:06 +0300

    Изменила путь до бэка

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,25 @@
+const dotenv = require('dotenv');
+dotenv.config({path: '.env.deploy'});
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'api-service',
+    script: './dist/app.js',
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/ritarixter/web-plus-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp ./.env ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
+};
