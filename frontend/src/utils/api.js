8da9e407b2a0250f8a1e28c15ac73920724642a8
Rita Rixter <ritarixter@mail.ru>
2023-02-17 21:37:47 +0300

    Исправила http на https

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('http://api.mesto.ritarixter.nomoredomainsclub.ru');
+  const api = new Api('https://api.mesto.ritarixter.nomoredomainsclub.ru');
 
   export default api;
